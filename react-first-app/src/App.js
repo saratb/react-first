a607eacf65e86aa6c61e8a7cc2dea3bf3cc493ce
sbondada <saratbondada2014@gmail.com>
2021-02-07 20:34:36 -0500

    Header Started Style

--- react-first-app/src/App.js
+++ react-first-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from './components/layout/Header'
 import Todos from './components/Todos';
 
 class App extends Component {
@@ -47,6 +48,7 @@ class App extends Component {
     console.log(this.state.todos);
     return ( //Return a JSX File // Adding a prop
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo = {this.delTodo}/>
       </div>
